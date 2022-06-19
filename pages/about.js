55ffed15608e00fc19495ffe91def62562adfa1e
Zak Mouchtati <zakaria.mouchtati01@gmail.com>
2022-06-19 12:33:38 +0100

    update the about page

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,14 @@
 import Link from 'next/link'
 import React from 'react'
+import Head from 'next/head'
 
 const about = () => {
   return (
     <div>
+        <Head>
+        <title>About Page</title>    
+        <meta property="og:image" content="https://centralesupelec-exed.ma/lean-leader/static/media/LeanLeader.57bc56db83cab695ddea.jpeg"/>
+        </Head>
         ABOUT PAGE
         <Link href="/" >Home Page</Link>
     </div>
