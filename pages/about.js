94fbc8ff676f4d9a5d7546fde26d448b4fed8aad
Zak Mouchtati <zakaria.mouchtati01@gmail.com>
2022-06-19 12:22:11 +0100

    first commit

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+import React from 'react'
+
+const about = () => {
+  return (
+    <div>
+        ABOUT PAGE
+        <Link href="/" >Home Page</Link>
+    </div>
+  )
+}
+
+export default about
